9fc4c5cb75b9fdc1a698fb39984a00b6d16dff7a
Ari Porad <ari@ariporad.com>
2022-10-11 22:02:17 -0400

    feat(hw4): finish practice.sv

--- hws/4/practice.sv
+++ hws/4/practice.sv
@@ -3,4 +3,48 @@
 
 module practice(rst, clk, ena, seed, out);
 
+	input wire rst, clk, ena, seed;
+	output logic out;
+
+	logic a, b, c, d;
+
+	/*
+
+	always_comb d = b ? a : 1'b0;
+	always_ff @(posedge c) begin
+		out[3] <= d;
+	end
+
+	*/
+
+	/*
+	// V1
+
+	always_comb b = ena ? a : seed;
+
+	// F1: b -> c
+	always_ff @(posedge clk) c = rst ? 1'b0 : b;
+
+	always_comb a = c ^ d;
+
+	// F2: c -> d
+	always_ff @(posedge clk) d = rst ? 1'b0 : c;
+
+	// F3: d -> out
+	always_ff @(posedge clk) out = rst ? 1'b0 : d;
+
+	*/
+
+	// V2
+	always_comb begin
+		a = c ^ d;
+		b = ena ? a : seed;
+	end
+
+	always_ff @(posedge clk) begin
+		out = rst ? 1'b0 : d;
+		d = rst ? 1'b0 : c;
+		c = rst ? 1'b0 : b;
+	end
+
 endmodule
