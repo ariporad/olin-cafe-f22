0ad898e9041176fb97826e6091c2306d878448cd
Ari Porad <ari@ariporad.com>
2022-12-09 01:33:34 -0500

    docs(asm/_preamble.s): document preamble and use li

--- labs/03_rv32i_part2/asm/_preamble.s
+++ labs/03_rv32i_part2/asm/_preamble.s
@@ -1,9 +1,28 @@
-PREAMBLE: # This is the only thing added from CE
-        # Set 4 high bits of sp to 0b0011 (bottom of data memory)
-        li sp, 3
-        slli sp, sp, 16
-        # Now make sp actually start at the top of the data memory (1024 words * 4 bytes):
-        addi sp, sp, 1
-        slli sp, sp, 12
-        call main
-        halt
+PREAMBLE:
+	# This will always be the first instruction, so it's where execution will begin
+
+	# First, we need to initialize the stack pointer
+	# Our MMU uses the highest 4 bits of the address to differentiate memory banks (see memmap.sv),
+	# so data memory addresses look like:
+	# 	0b0011xxxxxxxxxxxxxxxxxxxxxxxxxxxx
+	# The first one is:
+	# 	0b00110000000000000000000000000000
+	#
+	# However, we need our stack pointer to start at the *top* of data memory, so the stack can grow
+	# downwards (and the heap grows upwards). We have 1024 words of data memory (again, see
+	# memmap.sv), and a word is 32-bits long, or 4 bytes, so we have 4096 bytes of data memory total.
+	# Note that our memory is byte-addressable.
+	#
+	# This gives us the following for the top of data memory (and therefore the starting value of
+	# our stack pointer):
+	# 	0b00110000000000000001000000000000
+	#
+
+	li sp, 0b00110000000000000001000000000000
+
+	# Now we can call main
+	# By using a proper call (pseudo-)instruction, ra will be set so main can return like normal
+    call main
+
+	# Once main returns, we'll come back here and halt the CPU
+    halt
